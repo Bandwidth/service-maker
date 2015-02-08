de2037d73a42b2bc85b5169b949a1969373a8b5d
Justin Toniazzo <jutonz42@gmail.com>
2015-02-08 15:46:08 -0500

    Initial CLI and resource creation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Hapi = require('hapi');
 var Good = require('good');
 var request = require('request');
+var joi = require('joi');
 
 var server = new Hapi.Server();
 server.connection({
@@ -14,6 +15,35 @@ server.route({
 , handler: function(request, reply) {
     reply();
   }
+});
+
+server.route({
+  method: 'POST'
+, path: '/new'
+, handler: function(request, reply) {
+    var payload = JSON.parse(request.payload);
+    // reply(payload).code(201);
+
+    if (payload.timeToLive === undefined || payload.size === undefined || payload.sshKeypair === undefined) {
+      reply().code(400);
+    } else {
+      var awsResource = {
+        ipAddress: '8.8.8.8'
+      , hostname: 'ServiceMaker'
+      , username: 'Tyler'
+      , sshPrivateKey: payload.sshKeypair
+      };
+      reply(awsResource).code(201);
+    }
+  }
+// , config: {
+//     validate: {
+//       payload: {
+//         // timeToLive: joi.string().required()
+
+//       }
+//     }
+//   }
 });
 
 if (!module.parent) { // Don't start server if testing
