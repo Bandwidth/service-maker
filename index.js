46592facdeb34270dc4e0b8b277e325df63e137e
Justin Toniazzo <jutonz42@gmail.com>
2015-02-09 15:28:00 -0500

    Using Joi for input validation

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var Hapi = require('hapi');
 var Good = require('good');
 var request = require('request');
-var joi = require('joi');
+var Joi = require('joi');
+var Boom = require('boom');
 
 var server = new Hapi.Server();
 server.connection({
@@ -19,20 +20,24 @@ server.route({
 
 server.route({
   method: 'POST'
-, path: '/new'
+, path: '/v1/instances'
 , handler: function(request, reply) {
     var payload = JSON.parse(request.payload);
 
-    if (payload.timeToLive === undefined || payload.size === undefined || payload.sshKeypair === undefined) {
-      reply().code(400);
+    // This is a Joi schema
+    var INSTANCE_SCHEMA = Joi.object().keys({
+      timeToLive: Joi.number().integer().min(0), // duration in seconds
+      size: Joi.string().valid('micro', 'small', 'medium', 'large').default('micro'),
+      sshKeyPair: Joi.string().required()
+    });
+
+    // Use the schema to validate a payload
+    var validated = Joi.validate(request.payload, INSTANCE_SCHEMA);
+    if (validated.error) {
+      reply(Boom.badRequest(validated.error.message));
     } else {
-      var awsResource = {
-        ipAddress: '8.8.8.8'
-      , hostname: 'ServiceMaker'
-      , username: 'Tyler'
-      , sshPrivateKey: payload.sshKeypair
-      };
-      reply(awsResource).code(201);
+      var instance = validated.value;
+      reply(instance).code(201);
     }
   }
 });
