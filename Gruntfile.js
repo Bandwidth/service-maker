b3d3cda4b4c680421abd5af1ae29fb23c93e1170
Justin Toniazzo <jutonz42@gmail.com>
2015-02-12 14:41:29 -0500

    Fixed some style issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,65 +1,45 @@
-// module.exports = function(grunt) {
-//   grunt.initConfig({
-//     pkg: grunt.file.readJSON('package.json')
-//   , jshint: {
-//       files: ['Gruntfile.js', '*.js', 'test/*.js', 'lib/*.js']
-//     , options: {
-//         laxcomma: true
-//       }
-//     }
-//   , jscs: {
-//       files: [ '*.js', 'lib/*.js', 'test/*.js' ]
-//     }
-//   });
-
-//   grunt.loadNpmTasks('grunt-contrib-jshint');
-//   grunt.loadNpmTasks('grunt-jscs');
-
-//   grunt.registerTask('default', ['jshint', 'jscs']);
-// };
-
 module.exports = function(grunt) {
 
   var _ = grunt.util._;
 
-  var sourceFiles = [ "*.js", "app/**/*.js", "core/**/*.js", "travis/**/*.js" ];
-  var testFiles   = [ "test/**/*.js" ];
-  var allFiles    = sourceFiles.concat(testFiles);
+  var sourceFiles = [ '*.js', 'app/**/*.js', 'core/**/*.js', 'travis/**/*.js' ];
+  var testFiles = [ 'test/**/*.js' ];
+  var allFiles = sourceFiles.concat(testFiles);
 
   grunt.initConfig({
-    jscs : {
-      src     : allFiles,
-      options : {
-        config : ".jscsrc"
+    jscs: {
+      src: allFiles
+    , options : {
+        config : '.jscsrc'
       }
-    },
+    }
 
-    jshint : {
-      src     : sourceFiles,
-      options : {
+  , jshint : {
+      src     : sourceFiles
+    , options : {
         laxcomma: true
       }
-    },
+    }
 
-    mochaIstanbul : {
+  , mochaIstanbul : {
       coverage : {
-        src : "test"
+        src : 'test'
       }
     }
   });
 
   // Load plugins
-  grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-jscs");
-  grunt.loadNpmTasks("grunt-mocha-istanbul");
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jscs');
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
 
   // Rename tasks
-  grunt.task.renameTask("mocha_istanbul", "mochaIstanbul");
+  grunt.task.renameTask('mocha_istanbul', 'mochaIstanbul');
 
   // Register tasks
-  grunt.registerTask("test", [ "mochaIstanbul:coverage" ]);
-  grunt.registerTask("lint", "Check for common code problems.", [ "jshint" ]);
-  grunt.registerTask("style", "Check for style conformity.", [ "jscs" ]);
-  grunt.registerTask("default", [ "lint", "style", "test" ]);
+  grunt.registerTask('test', [ 'mochaIstanbul:coverage' ]);
+  grunt.registerTask('lint', 'Check for common code problems.', [ 'jshint' ]);
+  grunt.registerTask('style', 'Check for style conformity.', [ 'jscs' ]);
+  grunt.registerTask('default', [ 'lint', 'style', 'test' ]);
 
 };
