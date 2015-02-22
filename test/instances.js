79616d6570563c7d1f8c34d487bcf2c8afbf56c1
Justin Toniazzo <jutonz42@gmail.com>
2015-02-22 17:21:24 -0500

    Removed some tests until we sort authentication

--- test/instances.js
+++ test/instances.js
@@ -13,34 +13,6 @@ describe('GET /v1/instances', function() {
   // testing will work here.
   //
 
-  // it('should be able to get all instances', function(done) {
-  //   var request = {
-  //     url: '/v1/instances/'
-  //   , method: 'GET'
-  //   };
-  //   this.server.inject(request, function(response) {
-  //     should.exist(response);
-  //     console.log(response.payload);
-  //     response.statusCode.should.equal(200);
-  //     done();
-  //   });
-  // });
-
-  // it('should be able to retrieve info for instance i-12182a1e', function(done) {
-  //   var request = {
-  //     url: '/v1/instances/i-12182a1e'
-  //   , method: 'GET'
-  //   };
-  //   this.server.inject(request, function(response) {
-  //     should.exist(response);
-  //     response.statusCode.should.equal(200);
-  //     should.exist(response.payload.Tags);
-  //     response.payload.Tags[0].Key.should.equal('Name');
-  //     response.payload.Tags[0].Value.should.equal('Huehuehue')
-  //     done();
-  //   });
-  // });
-
 });
 
 describe('POST /v1/instances', function() {
