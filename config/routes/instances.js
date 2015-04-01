0c386447dff42fc20f1a1f1180ac6834db2837e0
Justin Toniazzo <jutonz42@gmail.com>
2015-04-01 11:15:41 -0400

    Applies TTL tags to instances on creation

--- config/routes/instances.js
+++ config/routes/instances.js
@@ -20,9 +20,9 @@ module.exports = [
 
       // Define an instance schema.
       var INSTANCE_SCHEMA = Joi.object().keys({
-        timeToLive: Joi.number().integer().min(0) // duration in seconds
-      , type: Joi.string().default('t1.micro')
+        type: Joi.string().default('t1.micro')
       , sshKeyPair: Joi.string().required()
+      , ttl: Joi.number().integer().min(1)
       });
 
       // Use the schema to validate a payload.
---
+++ test/InstancePool.js
@@ -0,0 +1,3 @@
+var InstancePool = require('./../lib/InstancePool');
+
+InstancePool.applyTtlTo('i-2e5ef1d8', 5);
