27be8d5ac1948171741f101d159665ae6cbe7f1e
Justin Toniazzo <jutonz42@gmail.com>
2015-04-17 11:22:02 -0400

    Start InstancePool tests

--- test/instance_pool_spec.js
+++ test/instance_pool_spec.js
@@ -1,11 +1,24 @@
+const touch = require('touch');
 const sinon = require('sinon');
 const should = require('chai').should();
 const proxyquire = require('proxyquire');
+var AWS = require('aws-sdk');
+var ec2 = new AWS.EC2({
+  apiVersion: '2014-10-01'
+, region: 'us-west-2'
+});
 
 var InstancePool = require('./../lib/InstancePool');
 var Helper = require('./test_helper');
 
+var pool;
 var stub;
+
+before(function(done) {
+  pool = new InstancePool();
+  pool.ec2 = ec2;
+  done();
+})
 
 after(function(done) {
   if (stub) { stub.restore(); }
@@ -16,16 +29,18 @@ describe('InstancePool', function() {
 
   describe('constructor', function() {
 
-    it('should accept a strategy paramater', function(done) {
+    it ('defaults to the NaiveStrategy', function(done) {
+      var pool = new InstancePool();
+      pool.strategy().should.equal('NaiveStrategy');
+      done();
+    })
 
-      var strategy = 'amazing_strategy';
-      var stubbedFunctions = {};
-      var strategyStub = ['./poolingStrategies', strategy].join('/');
-      console.log(strategyStub);
-      poolStub = proxyquire('./../lib/InstancePool', { strategyStub: stubbedFunctions });
-      pool = new poolStub('amazing_strategy');
+  })
 
-      stub.called.should.be.true();
+  describe('#startSshPolling', function() {
+
+    it('polls instances', function(done) {
+
       done();
     })
 
