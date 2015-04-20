762da6f44ca97995db797e72535e9ac281b31abb
Justin Toniazzo <jutonz42@gmail.com>
2015-04-20 11:33:48 -0400

    Finish instance pool test

--- test/instance_pool_spec.js
+++ test/instance_pool_spec.js
@@ -1,4 +1,3 @@
-const touch = require('touch');
 const sinon = require('sinon');
 const should = require('chai').should();
 const proxyquire = require('proxyquire');
@@ -15,7 +14,7 @@ var pool;
 var stub;
 var clock;
 
-before(function(done) {
+beforeEach(function(done) {
   pool = new InstancePool();
   pool.ec2 = ec2;
   done();
@@ -29,14 +28,112 @@ after(function(done) {
 
 describe('InstancePool', function() {
 
-  describe('constructor', function() {
+  it ('defaults to the NaiveStrategy', function(done) {
+    var pool = new InstancePool();
+    pool.strategy().should.equal('NaiveStrategy');
+    done();
+  })
 
-    it ('defaults to the NaiveStrategy', function(done) {
-      var pool = new InstancePool();
-      pool.strategy().should.equal('NaiveStrategy');
-      done();
-    })
+  it('initializes by creating instances', function(done) {
+    var instancesStub = {};
+    instancesStub.create = function(type, callback) {
+      callback(42);
+    }
+    instancesStub.handleTtl = function(instance) {}
+    var InstancePool = proxyquire('./../lib/InstancePool', { './Instances': instancesStub });
+    var pool = new InstancePool();
 
+    pool.getPoolInstancesByFilters = function(filters, callback) {
+      var instances = [];
+      instances.push({
+        InstanceType: 't2.micro'
+      , InstanceId: 42
+      });
+      callback(instances);
+    }
+    // stub = sinon.stub(Instances, 'handleTtl');
+
+    var createStub = sinon.stub(InstancePool, 'create');
+    var assimmilateStub = sinon.stub(InstancePool, 'assimilate');
+
+    pool.initialize();
+    // stub.called.should.be.true;
+    createStub.called.should.be.true;
+    assimmilateStub.called.should.be.true;
+    done();
+  })
+
+  it('initializes by destroying instances', function(done) {
+    var instancesStub = {};
+    instancesStub.create = function(type, callback) {
+      callback(42);
+    }
+    instancesStub.handleTtl = function(instance) {}
+    var InstancePool = proxyquire('./../lib/InstancePool', { './Instances': instancesStub });
+    var pool = new InstancePool();
+
+    pool.getPoolInstancesByFilters = function(filters, callback) {
+      var instances = [];
+      var fakeInstance = {
+        InstanceType: 't2.micro'
+      , InstanceId: 42
+      }
+      for (var i = 0; i < 5; i++) {
+        instances.push(fakeInstance);
+      }
+      callback(instances);
+    }
+    // stub = sinon.stub(Instances, 'handleTtl');
+
+    var createStub = sinon.stub(InstancePool, 'create');
+    var terminateStub = sinon.stub(InstancePool, 'terminate');
+    var assimmilateStub = sinon.stub(InstancePool, 'assimilate');
+
+    var spy = sinon.spy();
+    pool.initialize(undefined, spy);
+    spy.called.should.be.true;
+    createStub.called.should.be.true;
+    terminateStub.called.should.be.true;
+    assimmilateStub.called.should.be.true;
+    done();
+  })
+
+  it('logs to server while initializing', function(done) {
+    var instancesStub = {};
+    instancesStub.create = function(type, callback) {
+      callback(42);
+    }
+    instancesStub.handleTtl = function(instance) {}
+    var InstancePool = proxyquire('./../lib/InstancePool', { './Instances': instancesStub });
+    var pool = new InstancePool();
+
+    pool.getPoolInstancesByFilters = function(filters, callback) {
+      var instances = [];
+      var fakeInstance = {
+        InstanceType: 't2.micro'
+      , InstanceId: 42
+      }
+      for (var i = 0; i < 5; i++) {
+        instances.push(fakeInstance);
+      }
+      callback(instances);
+    }
+    // stub = sinon.stub(Instances, 'handleTtl');
+
+    var createStub = sinon.stub(InstancePool, 'create');
+    var terminateStub = sinon.stub(InstancePool, 'terminate');
+    var assimmilateStub = sinon.stub(InstancePool, 'assimilate');
+
+    var spy = sinon.spy();
+    var fakeServer = {};
+    fakeServer.log = sinon.spy();
+    pool.initialize(fakeServer, spy);
+    fakeServer.log.called.should.be.true;
+    spy.called.should.be.true;
+    createStub.called.should.be.true;
+    terminateStub.called.should.be.true;
+    assimmilateStub.called.should.be.true;
+    done();
   })
 
   describe('#startSshPolling', function() {
@@ -77,8 +174,102 @@ describe('InstancePool', function() {
 
   describe('#assimilate', function(done) {
 
-    it('adds sshable instances'), function(done) {
-      
+    it('adds sshable instances', function(done) {
+      var instancesStub = {};
+      instancesStub.handleTtl = function(instanceId) {
+        return;
+      }
+      instancesStub.canSsh = function(instanceId, callback) {
+        callback(true);
+      }
+      var InstancePool = proxyquire('./../lib/InstancePool', { './Instances': instancesStub })
+
+      const id = 42;
+      var spy = sinon.spy();
+      stub = sinon.stub(InstancePool, 'onSshable');
+      InstancePool.assimilate(id);
+      stub.called.should.be.true;
+      done();
+    })
+
+    it('adds non-sshable instances', function(done) {
+      var instancesStub = {};
+      instancesStub.handleTtl = function(instanceId) {
+        return;
+      }
+      instancesStub.canSsh = function(instanceId, callback) {
+        callback(false);
+      }
+      var InstancePool = proxyquire('./../lib/InstancePool', { './Instances': instancesStub })
+
+      const id = 42;
+      var spy = sinon.spy();
+      stub = sinon.stub(InstancePool, 'startSshPolling');
+      InstancePool.assimilate(id);
+      stub.called.should.be.true;
+      done();
+    })
+
+  })
+
+  describe('#createInstances', function() {
+
+    it('creates instances', function(done) {
+      stub = sinon.stub(InstancePool, 'create');
+      var instances = {};
+      instances['t2.micro'] = 1;
+
+      var spy = sinon.spy();
+      InstancePool.createInstances(instances, spy);
+      stub.called.should.be.true;
+      spy.called.should.be.true;
+      done();
+    })
+
+  })
+
+  describe('getPoolInstanccesByType', function() {
+
+    it('gets instances', function(done) {
+      pool.getPoolInstancesByFilters = function(filters, callback) {
+        callback();
+      }
+      spy = sinon.spy();
+      const type = 't1.micro';
+      pool.getPoolInstancesByType(type, spy);
+      spy.called.should.be.true;
+      done();
+    })
+
+  })
+
+  describe('getPoolInstances', function() {
+
+    it('returns undefined if no pool instances', function(done) {
+      pool.getPoolInstancesByFilters = function(filters, callback) {
+        callback(undefined);
+      }
+      var spy = sinon.spy();
+      pool.getPoolInstances(spy);
+      spy.called.should.be.true;
+      spy.calledWith(undefined).should.be.true;
+      done();
+    })
+
+    it('returns pool instances', function(done) {
+      pool.getPoolInstancesByFilters = function(filters, callback) {
+        var instances = [];
+        instances.push({
+          InstanceType: 't2.micro'
+        , InstanceId: 42
+        });
+        callback(instances);
+      }
+      var spy = sinon.spy();
+      pool.getPoolInstances(function(currentInstances) {
+        should.exist(currentInstances);
+        done();
+      });
     })
 
   })
