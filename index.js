582f5452bf699e121b97e192540a7a2ea7fc6a7f
unknown <amadan@amadan-PC.bandwidth.local>
2015-06-30 10:03:36 -0400

    Fixed style guidelines

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
+"use strict";
 //Modules required
-var Hapi = require('hapi');
+var Hapi = require("hapi");
 
 //Server initialization
 var server = new Hapi.Server();
 server.connection({
-	host: 'localhost',
-	port: 8000
+	host : "localhost",
+	port : 8000
 });
 
 //Adding routes to the server
 server.route({
-	method: 'GET',
-	path: '/',
-	handler: function (request, reply) {
-		reply('').code(200);
+	method  : "GET",
+	path    : "/",
+	handler : function (request, reply) {
+		reply("").code(200);
 	}
 
 });
 
 //Starting the server
 server.start(function () {
-	console.log('Server running at:', server.info.uri);
+	console.log("Server running at:", server.info.uri);
 });
