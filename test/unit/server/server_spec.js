60b94b730b5c41a3ca26c28588a8766cd84fb73a
Arjun Madan <arjunmadan1992@gmail.com>
2015-07-07 11:39:12 -0400

    Minor changes to rest.js

--- test/unit/server/server_spec.js
+++ test/unit/server/server_spec.js
@@ -21,31 +21,3 @@ describe("The server", function () {
 		expect(restStub.calledOnce, "registered").to.be.true;
 	});
 });
-
-describe("The Rest Plugin", function () {
-	var VALID_AMI     = "ami-default";
-	var VALID_TYPE    = "t2.micro";
-	//var INVALID_AMI   = [ "ami-defualt" ];
-	//var INVALID_TYPE  = [ "t2.micro" ];
-
-	describe("when a valid request is made", function () {
-		var server = new Hapi.Server();
-
-		before(function () {
-			server.connection();
-			return server.registerAsync(Rest);
-		});
-
-		it("creates the instance and returns the canonical url", function () {
-			var request = new Request("POST", "/v1/instances").mime("application/json").payload({
-				ami  : VALID_AMI,
-				type : VALID_TYPE
-			});
-			return request.inject(server)
-			.then(function (response) {
-				expect(response.statusCode, "status").to.equal(201);
-				//expect(response.location, "location").to.match(/v1/instances/);
-			});
-		});
-	});
-});
