cbad9f66a4ad3edc44ab0eaa6f89502fee7fa480
Arjun Madan <arjunmadan1992@gmail.com>
2015-07-07 15:19:25 -0400

    Rebasing

--- test/unit/plugins/rest_spec.js
+++ test/unit/plugins/rest_spec.js
@@ -9,6 +9,12 @@ var expect   = require("chai").expect;
 Bluebird.promisifyAll(Hapi);
 
 describe("The Rest plugin", function () {
+	var VALID_AMI     = "ami-default";
+	var VALID_TYPE    = "t2.micro";
+
+	var INVALID_AMI   = [ "ami-defualt" ];
+	//var INVALID_TYPE  = [ "t2.micro" ];
+
 	it("is a Hapi plugin", function () {
 		expect(Rest, "attributes").to.have.property("register")
 		.that.is.a("function")
@@ -31,4 +37,48 @@ describe("The Rest plugin", function () {
 			});
 		});
 	});
+
+	describe("when a valid request is made", function () {
+		var location = /\/v1\/instances\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}/;
+		var server = new Hapi.Server();
+
+		before(function () {
+			server.connection();
+			return server.registerAsync(Rest);
+		});
+
+		it("creates the instance and returns the canonical url", function () {
+			var request = new Request("POST", "/v1/instances").mime("application/json").payload({
+				ami  : VALID_AMI,
+				type : VALID_TYPE
+			});
+			return request.inject(server)
+			.then(function (response) {
+				expect(response.statusCode, "status").to.equal(201);
+				expect(response.headers.location, "location").to.match(location);
+			});
+		});
+	});
+
+	describe("when an invalid request is made", function () {
+		var server = new Hapi.Server();
+
+		before(function () {
+			server.connection();
+			return server.registerAsync(Rest);
+		});
+
+		it("returns an error", function () {
+			var request = new Request("POST", "/v1/instances").mime("application/json").payload({
+				ami  : INVALID_AMI,
+				type : VALID_TYPE
+			});
+			return request.inject(server)
+			.catch(function (err) {
+				console.log("here");
+				expect(err.statusCode, "status").to.equal(400);
+			});
+		});
+	});
+
 });
