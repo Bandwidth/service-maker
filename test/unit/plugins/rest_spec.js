2163199fcb996fb42ed1fb2e1e970512409e9ab6
unknown <rohitrp2003@gmail.com>
2015-07-08 16:08:38 -0400

    get rest api updated. Can send 500 error now

--- test/unit/plugins/rest_spec.js
+++ test/unit/plugins/rest_spec.js
@@ -6,6 +6,8 @@ var Hapi     = require("hapi");
 var Rest     = require("../../../lib/plugins/rest");
 var expect   = require("chai").expect;
 var Genesis  = require("genesis");
+var Sinon    = require("sinon");
+var Instance = require("../../../lib/services/instanceAdapter");
 
 require("sinon-as-promised")(Bluebird);
 Bluebird.promisifyAll(Hapi);
@@ -26,212 +28,269 @@ describe("The Rest plugin", function () {
 	});
 
 	describe("when registered", function () {
-		var server = new Hapi.Server();
+		var instances = new Instance(mapper);
 
 		before(function () {
 			server.connection();
-			return server.registerAsync(Rest);
-		});
-
-	before(function () {
-		server.connection();
-		return server.registerAsync({
-			register : Rest,
-			options  : {
-				mapper : mapper
-			}
-		});
-	});
-
-	describe("when registered", function () {
-		it("provides the '/' route", function () {
-			return new Request("GET", "/").inject(server)
-			.then(function (response) {
-				expect(response.statusCode, "status").to.equal(200);
-			});
-		});
-	});
-	it("creates the instance and returns the canonical url", function () {
-		var request = new Request("POST", "/v1/instances").mime("application/json").payload({
-			ami  : VALID_AMI,
-			type : VALID_TYPE
-		});
-		return request.inject(server)
-		.then(function (response) {
-			var payload;
-			expect(response.statusCode, "status").to.equal(201);
-
-			payload           = JSON.parse(response.payload);
-			VALID_INSTANCE_ID = payload.id;
-		});
-	});
-	describe("getting an invalid instance", function () {
-		var result;
-
-		before(function () {
-			return new Request("GET", "/v1/instances/" + INVALID_QUERY).inject(server)
-			.then(function (response) {
-				result = response;
+			return server.registerAsync({
+				register : Rest,
+				options  : {
+					mapper    : mapper,
+					instances : instances
+				}
 			});
 		});
 
-		it("shows the instance does not exist", function () {
-			expect(result.statusCode,"status").to.equal(404);
+		describe("when registered", function () {
+			it("provides the '/' route", function () {
+				return new Request("GET", "/").inject(server)
+				.then(function (response) {
+					expect(response.statusCode, "status").to.equal(200);
+				});
+			});
 		});
-	});
-
-	describe("getting a valid instance", function () {
-		var result;
-
-		before(function () {
-			return new Request("GET", "/v1/instances/" + VALID_INSTANCE_ID).inject(server)
+		it("creates the instance and returns the canonical url", function () {
+			var request = new Request("POST", "/v1/instances").mime("application/json").payload({
+				ami  : VALID_AMI,
+				type : VALID_TYPE
+			});
+			return request.inject(server)
 			.then(function (response) {
-				result = response;
+				var payload;
+				expect(response.statusCode, "status").to.equal(201);
+
+				payload           = JSON.parse(response.payload);
+				VALID_INSTANCE_ID = payload.id;
+			});
+		});
+		describe("getting an invalid instance", function () {
+			var result;
+
+			before(function () {
+				return new Request("GET", "/v1/instances/" + INVALID_QUERY).inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
+
+			it("shows the instance does not exist", function () {
+				expect(result.statusCode,"status").to.equal(404);
 			});
 		});
 
-		it("shows the instance", function () {
-			expect(result.statusCode,"status").to.equal(200);
-		});
-	});
+		describe("getting a valid instance", function () {
+			var result;
 
-	describe("getting a list of valid instances", function () {
-		var result;
+			before(function () {
+				return new Request("GET", "/v1/instances/" + VALID_INSTANCE_ID).inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
 
-		before(function () {
-			return new Request("GET", "/v1/instances?type=" + VALID_TYPE).inject(server)
-			.then(function (response) {
-				result = response;
+			it("shows the instance", function () {
+				expect(result.statusCode,"status").to.equal(200);
 			});
 		});
 
-		it("returns an array of instances", function () {
-			var payload;
-			expect(result.statusCode,"status").to.equal(200);
-			payload = JSON.parse(result.payload);
-			expect(payload.instances).to.have.length.of.at.least(1);
-		});
-	});
+		describe("getting a list of valid instances", function () {
+			var result;
 
-	describe("getting an invalid instance by sending non-exisitng Instance Id and type", function () {
-		var result;
+			before(function () {
+				return new Request("GET", "/v1/instances?type=" + VALID_TYPE).inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
 
-		before(function () {
-			return new Request("GET", "/v1/instances?type=" + INVALID_QUERY + "&id=" + INVALID_QUERY)
-			.inject(server)
-			.then(function (response) {
-				result = response;
+			it("returns an array of instances", function () {
+				var payload;
+				expect(result.statusCode,"status").to.equal(200);
+				payload = JSON.parse(result.payload);
+				expect(payload.instances).to.have.length.of.at.least(1);
 			});
 		});
 
-		it("returns an empty array of instances", function () {
-			var payload;
-			expect(result.statusCode,"status").to.equal(200);
-			payload = JSON.parse(result.payload);
-			expect(payload.instances.length).equal(0);
-		});
-	});
+		describe("getting an invalid instance by sending non-exisitng Instance Id and type", function () {
+			var result;
 
-	describe("Sending a GET request with all invalid parameters", function () {
-		var result;
+			before(function () {
+				return new Request("GET", "/v1/instances?type=" + INVALID_QUERY + "&id=" + INVALID_QUERY)
+				.inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
 
-		before(function () {
-			return new Request("GET", "/v1/instances?extra=test&random=thisiswrong")
-			.inject(server)
-			.then(function (response) {
-				result = response;
+			it("returns an empty array of instances", function () {
+				var payload;
+				expect(result.statusCode,"status").to.equal(200);
+				payload = JSON.parse(result.payload);
+				expect(payload.instances.length).equal(0);
 			});
 		});
 
-		it("returns a Bad Request", function () {
-			expect(result.statusCode,"status").to.equal(400);
-		});
-	});
+		describe("Sending a GET request with all invalid parameters", function () {
+			var result;
 
-	describe("Sending a GET request with a mix of valid and invalid parameters", function () {
-		var result;
+			before(function () {
+				return new Request("GET", "/v1/instances?extra=test&random=thisiswrong")
+				.inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
 
-		before(function () {
-			return new Request("GET", "/v1/instances?extra=test&random=thisiswrong&type=" + VALID_TYPE)
-			.inject(server)
-			.then(function (response) {
-				result = response;
+			it("returns a Bad Request", function () {
+				expect(result.statusCode,"status").to.equal(400);
 			});
 		});
 
-		it("returns a Bad Request", function () {
-			expect(result.statusCode,"status").to.equal(400);
-		});
-	});
+		describe("Sending a GET request with a mix of valid and invalid parameters", function () {
+			var result;
 
-	describe("Sending a GET request with a valid ID", function () {
-		var result;
+			before(function () {
+				return new Request("GET", "/v1/instances?extra=test&random=thisiswrong&type=" + VALID_TYPE)
+				.inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
 
-		before(function () {
-			return new Request("GET", "/v1/instances?id=" + VALID_INSTANCE_ID)
-			.inject(server)
-			.then(function (response) {
-				result = response;
+			it("returns a Bad Request", function () {
+				expect(result.statusCode,"status").to.equal(400);
 			});
 		});
 
-		it("returns an instance with requested id", function () {
-			var payload;
-			expect(result.statusCode,"status").to.equal(200);
-			payload = JSON.parse(result.payload);
-			expect(payload.instances.length).equal(1);
-			expect(payload.instances[ 0 ].id).to.equal(VALID_INSTANCE_ID);
-		});
-	});
+		describe("Sending a GET request with a valid ID", function () {
+			var result;
 
-	describe("Sending a GET request with a valid AMI", function () {
-		var result;
+			before(function () {
+				return new Request("GET", "/v1/instances?id=" + VALID_INSTANCE_ID)
+				.inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
 
-		before(function () {
-			return new Request("GET", "/v1/instances?ami=" + VALID_AMI)
-			.inject(server)
-			.then(function (response) {
-				result = response;
+			it("returns an instance with requested id", function () {
+				var payload;
+				expect(result.statusCode,"status").to.equal(200);
+				payload = JSON.parse(result.payload);
+				expect(payload.instances.length).equal(1);
+				expect(payload.instances[ 0 ].id).to.equal(VALID_INSTANCE_ID);
 			});
 		});
 
-		it("returns an array of instances with requested ami", function () {
-			expect(result.statusCode,"status").to.equal(200);
-		});
-	});
+		describe("Sending a GET request with a valid AMI", function () {
+			var result;
 
-	describe("Sending a GET request with state=pending", function () {
-		var result;
+			before(function () {
+				return new Request("GET", "/v1/instances?ami=" + VALID_AMI)
+				.inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
 
-		before(function () {
-			return new Request("GET", "/v1/instances?state=pending")
-			.inject(server)
-			.then(function (response) {
-				result = response;
+			it("returns an array of instances with requested ami", function () {
+				expect(result.statusCode,"status").to.equal(200);
 			});
 		});
 
-		it("returns an array of instances which are in a pending state", function () {
-			expect(result.statusCode,"status").to.equal(200);
-		});
-	});
+		describe("Sending a GET request with state=pending", function () {
+			var result;
 
-	describe("Sending a GET request an invalid URI", function () {
-		var result;
+			before(function () {
+				return new Request("GET", "/v1/instances?state=pending")
+				.inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
 
-		before(function () {
-			return new Request("GET", "/v1/instances?uri=" + INVALID_QUERY)
-			.inject(server)
-			.then(function (response) {
-				result = response;
+			it("returns an array of instances which are in a pending state", function () {
+				expect(result.statusCode,"status").to.equal(200);
 			});
 		});
 
-		it("returns an empty array of intsances", function () {
-			var payload;
-			expect(result.statusCode,"status").to.equal(200);
-			payload = JSON.parse(result.payload);
-			expect(payload.instances.length).equal(0);
+		describe("Sending a GET request an invalid URI", function () {
+			var result;
+
+			before(function () {
+				return new Request("GET", "/v1/instances?uri=" + INVALID_QUERY)
+				.inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
+
+			it("returns an empty array of intsances", function () {
+				var payload;
+				expect(result.statusCode,"status").to.equal(200);
+				payload = JSON.parse(result.payload);
+				expect(payload.instances.length).equal(0);
+			});
+		});
+
+		describe("failing to find an instance for specified instanceId", function () {
+			var result;
+/*
+			before(function () {
+				Sinon.stub(mapper, "findOne", function () {
+					return Bluebird.reject(new Error("Simulated Failure."));
+				});
+				return new Request("GET", "/v1/" + VALID_INSTANCE_ID).inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
+
+			after(function () {
+				mapper.findOne.restore();
+			});
+
+			it("displays an error page", function () {
+				expect(result.statusCode).to.equal(500);
+			});*/
+			before(function () {
+				Sinon.stub(instances, "getInstance").rejects(new Error("Simulated Failure"));
+
+				return new Request("GET", "/v1/instances/foo-bar-baz").inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
+
+			after(function () {
+				instances.getInstance.restore();
+			});
+
+			it("fails", function () {
+				expect(result.statusCode).to.equal(500);
+			});
+		});
+
+		describe("failing to find all instances", function () {
+			var result;
+
+			before(function () {
+				Sinon.stub(mapper, "find", function () {
+					return Bluebird.reject(new Error("Simulated Failure."));
+				});
+				return new Request("GET", "/v1/instances?id=" + VALID_INSTANCE_ID).inject(server)
+				.then(function (response) {
+					result = response;
+				});
+			});
+
+			after(function () {
+				mapper.find.restore();
+			});
+
+			it("fails", function () {
+				expect(result.statusCode).to.equal(500);
+			});
 		});
 	});
 });
