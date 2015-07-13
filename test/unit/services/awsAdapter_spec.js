51e2aa45cbf6734de0062516cd9e0f5b63a6e5d2
Arjun Madan <arjunmadan1992@gmail.com>
2015-07-13 13:52:38 -0400

    Now creates actual instances on AWS. Test cases need to be written

--- test/unit/services/awsAdapter_spec.js
+++ test/unit/services/awsAdapter_spec.js
@@ -10,7 +10,10 @@ describe("The AwsAdapter class ", function () {
 	var DEFAULT_IMAGE_ID      = "ami-d05e75b8";
 	var DEFAULT_INSTANCE_TYPE = "t2.micro";
 
-	describe("creating a new instance", function () {
+	var INVALID_IMAGE_ID      = "ami-invalid";
+	var INVALID_INSTANCE_TYPE = "t2.invalid";
+
+	describe("creating a new instance with valid ami, type", function () {
 		var awsAdapterStub;
 		var result;
 		var awsAdapter = new AwsAdapter({
@@ -19,7 +22,6 @@ describe("The AwsAdapter class ", function () {
 		});
 
 		before(function () {
-			//awsAdapter.runInstances();
 			awsAdapterStub = Sinon.stub(awsAdapter, "runInstances").onFirstCall()
 			.returns(Bluebird.resolve("test"));
 
@@ -27,15 +29,69 @@ describe("The AwsAdapter class ", function () {
 			.then(function (response) {
 				result = response;
 			});
-
 		});
 
 		after(function () {
 			//awsAdapterStub.restore();
 		});
 
-		it("returns a new instance with default values", function () {
-			console.log(awsAdapterStub);
+		it("returns a new instance with the ami and type provided", function () {
+			expect(result, "response").to.equal("test");
+		});
+
+	});
+
+	describe("creating a new instance with a invalid ami", function () {
+		var awsAdapterStub;
+		var result;
+		var awsAdapter = new AwsAdapter({
+			id   : INVALID_IMAGE_ID,
+			type : DEFAULT_INSTANCE_TYPE
+		});
+
+		before(function () {
+			awsAdapterStub = Sinon.stub(awsAdapter, "runInstances").onFirstCall()
+			.returns(Bluebird.resolve("test"));
+
+			awsAdapter.runInstances()
+			.then(function (response) {
+				result = response;
+			});
+		});
+
+		after(function () {
+			//awsAdapterStub.restore();
+		});
+
+		it("returns a new instance with the ami and type provided", function () {
+			expect(result, "response").to.equal("test");
+		});
+
+	});
+
+	describe("creating a new instance with an invalid type", function () {
+		var awsAdapterStub;
+		var result;
+		var awsAdapter = new AwsAdapter({
+			id   : DEFAULT_IMAGE_ID,
+			type : INVALID_INSTANCE_TYPE
+		});
+
+		before(function () {
+			awsAdapterStub = Sinon.stub(awsAdapter, "runInstances").onFirstCall()
+			.returns(Bluebird.resolve("test"));
+
+			awsAdapter.runInstances()
+			.then(function (response) {
+				result = response;
+			});
+		});
+
+		after(function () {
+			//awsAdapterStub.restore();
+		});
+
+		it("returns a new instance with the ami and type provided", function () {
 			expect(result, "response").to.equal("test");
 		});
 
