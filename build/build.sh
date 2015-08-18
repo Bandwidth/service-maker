d8fbd7fae660b1692e1ff12a67d1552e8cd2017e
unknown <rohitrp2003@gmail.com>
2015-08-18 11:48:52 -0400

    update travis

--- build/build.sh
+++ build/build.sh
@@ -2,12 +2,14 @@
 BASEDIR=`dirname ${BASH_SOURCE[0]}`
 PROJECT_DIRECTORY="${BASEDIR}/.."
 
+echo "1"
 # Run the unit tests.
 pushd "${PROJECT_DIRECTORY}" > /dev/null
-npm install
+echo "2"
 npm test
+echo "3"
 popd > /dev/null
-
+echo "4"
 # Run the integration tests.
 pushd "${PROJECT_DIRECTORY}" > /dev/null
 npm run-script integration-tests
